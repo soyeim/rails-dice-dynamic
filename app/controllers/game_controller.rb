43e3763158f467c3892479e68975520f224e2678
soyeim <146144729+soyeim@users.noreply.github.com>
2023-10-30 07:39:26 +0000

    erb and routes

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,19 @@
+class GameController < ApplicationController
+  def home
+    render({:template => "my_templates/home"})
+  end
+
+  def roll
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+
+    @rolls = []
+
+    @num_dice.times do 
+      die = rand(1..@sides)
+      
+      @rolls.push(die)
+    end
+    render({:template => "my_templates/roll"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
 
+  get("/",{:controller => "game", :action => "home"})
+  get("/dice/:number_of_dice/:how_many_sides", {:controller => "game", :action => "roll"})
 end
